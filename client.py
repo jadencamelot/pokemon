060dfef29caf483dcf79fd890509c561908fbc83
Jaden Burt <jaden.burt@cba.com.au>
2019-08-24 14:22:12 +1000

    Refactor client initialisation

--- client.py
+++ client.py
@@ -1,14 +1,16 @@
 import requests
 
+MAX_POKEMON = 964
+
 class Client():
     URL = "https://pokeapi.co/api/v2"
 
     def __init__(self):
-        self._setup()
+        self.pokemon = self._get_list_of_pokemon(MAX_POKEMON)
 
-    def _setup(self):
-        r = requests.get(f"{self.URL}/pokemon")
-        self.pokemon = r.json()['results']
+    def _get_list_of_pokemon(self, limit=20)
+        r = requests.get(f"{self.URL}/pokemon?limit={limit}")
+        return r.json()['results']
 
 if __name__ == "__main__":
     c = Client()
