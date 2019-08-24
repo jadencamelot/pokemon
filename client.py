a14492a217c418aae1e8d9820d8a777e88befda9
Jaden Burt <jaden.burt@cba.com.au>
2019-08-24 15:15:29 +1000

    Add list, search and count functionality. Refactor main loop. Refactor client.

--- client.py
+++ client.py
@@ -1,17 +1,49 @@
 import requests
 
-MAX_POKEMON = 964
 
 class Client():
     URL = "https://pokeapi.co/api/v2"
+    MAX_POKEMON = 964
 
     def __init__(self):
-        self.pokemon = self._get_list_of_pokemon(MAX_POKEMON)
+        self._pokemon = self._get_list_of_pokemon(self.MAX_POKEMON)
 
-    def _get_list_of_pokemon(self, limit=20)
+    def _get_list_of_pokemon(self, limit=20):
+        print("Loading list of pokemon...")
+
         r = requests.get(f"{self.URL}/pokemon?limit={limit}")
-        return r.json()['results']
+        results = r.json()['results']
+
+        # Parse results
+        return self._parse_pokemon_list(results)
+
+    def _parse_pokemon_list(self, pokemon_list):
+        parsed_pokemon = {}
+        for pokemon in pokemon_list:
+            name = pokemon['name']
+            url = pokemon['url']
+            parsed_pokemon[name] = url
+        return parsed_pokemon
+
+    def pokemon_list(self):
+        return self._pokemon.keys()
+
+    def pokemon_count(self):
+        return len(self._pokemon)
+
+    def pokemon_search(self, pokemon_name):
+        if pokemon_name not in self._pokemon:
+            return None
+
+        url = self._pokemon[pokemon_name]
+        pokemon_info = self._get_pokemon_info(url)
+        return pokemon_info
+
+    def _get_pokemon_info(self, url):
+        r = requests.get(url)
+        return r.json()
+
 
 if __name__ == "__main__":
     c = Client()
-    print(c.pokemon)
+    print(f"{str(len(c._pokemon))} pokemon loaded successfully.")
--- main.py
+++ main.py
@@ -2,19 +2,56 @@
 
 from client import Client
 
+
 def main():
     exit_flag = False
     c = Client()
 
-    while exit_flag == False:
-        command = input("Enter command: ")
-        if command == "hello":
-            print("Hello World!")
-        elif command == "exit":
+    while exit_flag is False:
+        command = input("Enter command: ").lower()
+        if command == "exit":
             exit_flag = True
-        elif command == "pokemon":
-            n_pokemon = len(c.pokemon)
-            print(f"There are {n_pokemon} pokemon.")
+        elif command == "list":
+            list_pokemon(c)
+        elif command == "count":
+            count(c)
+        elif command == "search":
+            search(c)
+
+
+def list_pokemon(c):
+    for i, pokemon_name in enumerate(c.pokemon_list()):
+        print(f"{str(i+1)}. {pokemon_name}")
+
+
+def count(c):
+    n_pokemon = c.pokemon_count()
+    print(f"There are {str(n_pokemon)} pokemon.")
+
+
+def search(c):
+    search_term = input("Pokemon name: ")
+    search_result = c.pokemon_search(search_term)
+    if search_result:
+        abilities = search_result['abilities']
+        pretty_print_abilities(abilities)
+    else:
+        print(f"Pokemon {search_term} not found!")
+
+
+def pretty_print_abilities(abilities):
+    print("\n-- Abilities --\n")
+    for ability in abilities:
+        is_hidden = ability['is_hidden']
+        if is_hidden:
+            hidden_str = "(hidden)"
+        else:
+            hidden_str = ""
+
+        print(f"Name: {ability['ability']['name'].capitalize()} {hidden_str}")
+        print(f"  Slot: {ability['slot']}")
+    print()
+
 
 if __name__ == "__main__":
     main()
